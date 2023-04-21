368e807a4d9b55fdea3d94ff0ae99bb3766769e5
OLUWASEGUN MOSES OLOPADE <mose14real@gmail.com>
2023-04-21 15:39:28 +0100

    Bug fixed

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,5 @@
 const projectType = document.querySelector('.project-type');
-// const projectTypeUser = document.querySelector('.project-type-user');
 const groupDetails = document.querySelector('.group-details');
-// const groupDetailsUser = document.querySelector('.group-details-user');
-
 
 projectType.addEventListener('click', (event)=>{
     event.preventDefault();
@@ -13,13 +10,3 @@ projectType.addEventListener('click', (event)=>{
         groupDetails.classList.add('d-none');
     }
 })
-
-// projectTypeUser.addEventListener('click', (event)=>{
-//     event.preventDefault();
-//     if(projectTypeUser.value == 'group')
-//     {
-//         groupDetailsUser.classList.remove('d-none');
-//     }else if(projectTypeUser.value == 'individual'){
-//         groupDetailsUser.classList.add('d-none');
-//     }
-// })
